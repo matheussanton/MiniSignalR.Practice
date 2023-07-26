6e84e4239d136eb5b29b9c60f8db3255dbfda4df
matheussanton <matheussantong@gmail.com>
2023-07-26 16:59:28 -0300

    Initial commit

---
+++ MiniSignalR.Service/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<MyHub>("/chat");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+class MyHub : Hub
+{
+    public async IAsyncEnumerable<DateTime> Stream(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            yield return DateTime.Now;
+            await Task.Delay(1000, cancellationToken);
+        }
+    }
+}
